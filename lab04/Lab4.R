bdda7409023c8ebf0a2490fc568997d40ce7df00
uzmaaly12 <89941843+uzmaaly12@users.noreply.github.com>
2021-11-11 23:43:05 -0600

    lab04

---
+++ lab04/Lab4.R
@@ -0,0 +1,100 @@
+library(tidyverse)
+library(sf)
+library(raster)
+library(dplyr)
+library(spData)
+library(spDataLarge)
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(ggplot2) # tidyverse data visualization package
+library(GISTools)
+
+countyboundries <- sf::read_sf("../data/County_Boundaries-_Census.shp")%>% sf::st_make_valid() 
+NE.counties <- read.csv(file = '../data/ne_counties.csv')
+CountyData <- sf::read_sf("../data/lancaster_county.shp")
+MuncData <-  sf::read_sf( "../data/Municipal_Boundaries.shp")%>% sf::st_make_valid() 
+StatePark <- sf::read_sf("../data/State_Park_Locations.shp")%>% sf::st_make_valid()
+StreamData <-  sf::read_sf ("../data/Streams_303_d_.shp")%>% sf::st_make_valid()
+Dem <- raster("../data/lc_dem.tif") 
+
+#Task 1................................ Frankenmap
+
+#State data frame:
+
+FemalePop.NE <-  NE.counties %>% mutate(FemalePop = (Female/Total)*100)
+join_tbl <- merge(countyboundries, FemalePop.NE, by.x= "NAME10", by.y = "NAME10" ) 
+NE.map <- tm_shape(join_tbl) + tm_fill(col = "FemalePop", alpha = 0.9, style="quantile") +
+  tm_borders(col = "brown", lwd = 1.5, lty = "solid")+ tm_scale_bar(breaks = c(0, 50,100), text.size = 0.5, position = c("right", "top"),bg.color = "grey" )
+NE.map 
+
+#County data frame:
+
+M.Lancaster <- st_intersection(MuncData,CountyData)
+Stream.Lan <- st_intersection(StreamData,CountyData)
+
+Lancaster.map <- tm_shape(M.Lancaster)+
+  tm_fill("NAME",legend.show = FALSE, alpha = 0.7)+
+  tm_text("NAME")+ 
+  tm_shape(Stream.Lan)+
+  tm_lines("Impairment",legend.col.show=FALSE)+
+  tm_shape(StatePark)+
+  tm_symbols(shape = 8,col = "black")+
+  tm_layout(legend.position =c("right","center"))
+Lancaster.map
+
+# map <- base + layer_1 + layer_2 + aesthetic + function + layer_3
+
+tm_shape(countyboundries) + tm_fill() + tm_borders()
+Lancaster <- countyboundries[countyboundries$NAMELSAD10 == "Lancaster County",]
+Lancaster 
+
+tm_shape(StreamData) + tm_lines()
+tm_shape(StatePark) + tm_dots()
+tm_shape(MuncData) + tm_polygons()
+
+Lancaster.region = st_bbox(c(xmin = -96.91394, xmax = -96.46363,
+                             ymin = 40.52302, ymax = 41.04612),
+                           crs = st_crs(countyboundries)) %>% st_as_sfc()
+
+Inset.map <- tm_shape(Lancaster.region) + tm_borders(lwd = 5, col="red")
+
+# Putting the state map as an inset to the county map
+
+NE.map + Inset.map 
+
+# -Lancaster cOUNTY
+Lancaster.Mun <- sf::st_intersection(MuncData, Lancaster)
+Lancaster.parks <- sf::st_intersection(StatePark, Lancaster)
+Lancaster.streams <- sf::st_intersection(StreamData, Lancaster)
+
+raster::crs(Dem) <- crs(Lancaster)  
+
+# plot(Dem)
+
+lc.map <- tm_shape(Dem) + 
+  tm_raster(alpha = 0.7, palette = colorRampPalette(c("darkolivegreen4","yellow", "brown"))(12),
+  legend.show = F) + tm_compass(type = "8star", position = c("right", "top"), size = 2) +
+  tm_layout(main.title="Map of Lancaster County", title.size = 1.1)
+
+lc.map
+
+library(grid)
+lc.map + Lancaster.map
+print(NE.map + Inset.map, vp = viewport(0.85, 0.137, width = 0.3, height = 0.3))
+
+#Task 2...........................................Static Map
+
+GB.Dem <- raster("../data/Gb_dem.tif")
+Gb_boundry <- sf::read_sf("../data/Gilgit_new_distric_boundry.shp")%>% sf::st_make_valid() 
+River.GB <- sf::read_sf("../data/waterways.shp")%>% sf::st_make_valid() 
+
+GB.Dem <- aggregate(GB.Dem, fact =2)
+GB.Dem
+
+map_gb = tm_shape(Gb_boundry) + tm_polygons()
+map_gb
+
+map_gb1 = map_gb + tm_shape(GB.Dem) + tm_raster(alpha = 0.7)
+map_gb1
+
+
